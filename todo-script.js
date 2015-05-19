c6808173d1fdb5507213528251ac2e9d4be1d60e
kilbergr <rekilberg@gmail.com>
2015-05-18 17:27:19 -0700

    simple to-do, initial commit

---
+++ todo-script.js
@@ -0,0 +1,24 @@
+var form = document.querySelector("form");
+var addNew = document.getElementsByTagName("ul")
+var input = document.getElementById("list"); 
+
+form.addEventListener("click", function(event){
+	event.preventDefault();
+  	var newItem = document.createElement("LI");
+  	var text = document.createTextNode(input.value);
+  	newItem.appendChild(text);
+    addNew[0].appendChild(newItem);
+    document.getElementById("list").value='';
+});
+
+form.addEventListener("keydown", function(event){
+  	if (13 == event.keycode) {
+	   event.preventDefault();
+	  	var newItem = document.createElement("LI");
+	  	var text = document.createTextNode(input.value);
+	  	newItem.appendChild(text);  	
+	    addNew[0].appendChild(newItem)
+	    document.getElementById("list").value='';
+	}
+});
+
